83a4fe51f432cd5a8b11b95607a7103e9c163e1a
Ishan <ishan@Ishans-MacBook-Pro.local>
2023-09-23 07:20:46 +0530

    adding contexual summaries

--- tests/step-definitions/common-hooks.ts
+++ tests/step-definitions/common-hooks.ts
@@ -5,6 +5,7 @@ import {
 	chromium
 } from '@playwright/test'
 import { After, AfterAll, Before, BeforeAll } from '@cucumber/cucumber'
+import { Scenario } from "@cucumber/messages";
 
 let browser: ChromiumBrowser
 
@@ -18,19 +19,19 @@ Before(async function(this: ICustomWorld) {
 });
 
 After(async function(this: ICustomWorld) {
-  await this.page?.close()
-  await this.context?.close()
   await core.summary
     .addHeading('E2E test results')
     .addCodeBlock("Explore playwright", "js")
     .addTable([
       [{data: 'File', header: true}, {data: 'Result', header: true}],
-      ['foo.js', 'Pass ✅'],
+      [Scenario.name, 'Pass ✅'],
       ['bar.js', 'Fail ❌'],
       ['test.js', 'Pass ✅']
     ])
     .addLink('View staging deployment!', 'https://github.com')
     .write()
+  await this.page?.close()
+  await this.context?.close()
 });
   
 AfterAll(async function() {
