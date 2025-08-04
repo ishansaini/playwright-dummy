9ac7ef23d5ddb216a6847ef063fad230856a8880
Ishan Saini <ishansaini@protonmail.com>
2025-08-04 17:43:30 +0530

    fix: minor updates

--- tests/step-definitions/common-hooks.ts
+++ tests/step-definitions/common-hooks.ts
@@ -1,43 +1,34 @@
 import { ICustomWorld } from "./custom-world";
-import * as core from '@actions/core';
+import * as core from "@actions/core";
+import { ChromiumBrowser, chromium } from "@playwright/test";
 import {
-	ChromiumBrowser,
-	chromium
-} from '@playwright/test'
-import { After, AfterAll, AfterStep, Before, BeforeAll } from '@cucumber/cucumber'
+  After,
+  AfterAll,
+  AfterStep,
+  Before,
+  BeforeAll,
+} from "@cucumber/cucumber";
 
-let browser: ChromiumBrowser
+let browser: ChromiumBrowser;
 
-BeforeAll(async function() {
-    browser = await chromium.launch({ headless: true })
-  });
-  
-Before(async function(this: ICustomWorld, scenario) {
-  this.context = await browser.newContext()
-  this.page = await this.context.newPage()
+BeforeAll(async function () {
+  browser = await chromium.launch({ headless: true, slowMo: 2000 });
 });
 
-AfterStep(async function(this: ICustomWorld, scenario) {
+Before(async function (this: ICustomWorld, scenario) {
+  this.context = await browser.newContext();
+  this.page = await this.context.newPage();
+});
+
+AfterStep(async function (this: ICustomWorld, scenario) {
   console.log(scenario.pickleStep.text);
 });
 
-After(async function(this: ICustomWorld, scenario) {
-  await core.summary
-    .addHeading('E2E test results')
-    .addCodeBlock("Explore playwright", "js")
-    .addTable([
-      [{data: 'File', header: true}, {data: 'Result', header: true}],
-      [scenario.pickle.name, scenario.result?.status + ' ✅'],
-      ['bar.js', 'Fail ❌'],
-      ['test.js', 'Pass ✅']
-    ])
-    .addLink('View staging deployment!', 'https://github.com')
-    .write()
-
-  await this.page?.close()
-  await this.context?.close()
+After(async function (this: ICustomWorld, scenario) {
+  await this.page?.close();
+  await this.context?.close();
 });
-  
-AfterAll(async function() {
-  await browser.close()
+
+AfterAll(async function () {
+  await browser.close();
 });
